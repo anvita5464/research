66d9636720d2e736f0518d9fcfa34195dabcc803
Kawapeachy <tk3104@columbia.edu>
2025-03-13 16:25:45 -0400

    multiObjectTracker
    - Added multiObjectTracker to track bubbles
    - Inserted Cutom Function called 'displayTrackingResults'
    - Added bubble tracking with multiObjectTracker in for loop

--- s1_Bubble_Detection.m
+++ s1_Bubble_Detection.m
@@ -4,8 +4,6 @@ videoFilePath = 'C:\Users\ameyo\OneDrive\Desktop\Research\Data\Flow through elec
 outputFolder = 'C:\Users\ameyo\OneDrive\Desktop\Research\Data\Flow through electrolyzer\Cell v2 - bigger windows\2025-02-21-Ni-Nimesh-0p5Na2SO4-10mlmin-new_cell_footagealignment\Bubble Analysis'; % Replace with your output folder path
 DataAnalysisFolder = 'C:\Users\ameyo\OneDrive\Desktop\Research\Data\Flow through electrolyzer\Cell v2 - bigger windows\2025-02-21-Ni-Nimesh-0p5Na2SO4-10mlmin-new_cell_footagealignment\Data Analysis';
 
-# Hola
-
 if ~exist(outputFolder, 'dir')
     mkdir(outputFolder);
 end
@@ -32,7 +30,48 @@ frame = readFrame(video);
 frames = zeros(100,size(frame,1),size(frame,2));
 means = uint8(squeeze(mean(frames,1)));
 
-tracker = multiObjectTracker();
+tracker = multiObjectTracker('MaxNumTracks', 1500, 'AssignmentThreshold', 30,  'ConfirmationThreshold', [8 10], DeletionThreshold= 20);
+
+
+% --------- Custom Function ------------ %
+function displayTrackingResults(videoPlayer,confirmedTracks,frame)
+    if ~isempty(confirmedTracks)
+        % Display the objects. If an object has not been detected
+        % in this frame, display its predicted bounding box.
+        numRelTr = numel(confirmedTracks);
+        boxes = zeros(numRelTr,4);
+        ids = zeros(numRelTr,1, 'int32');
+        predictedTrackInds = zeros(numRelTr,1);
+        for tr = 1:numRelTr
+            % Get bounding boxes.
+            boxes(tr,:) = confirmedTracks(tr).ObjectAttributes.BoundingBox;
+
+            % Get IDs.
+            ids(tr) = confirmedTracks(tr).TrackID;
+
+            if confirmedTracks(tr).IsCoasted
+                predictedTrackInds(tr) = tr;
+            end
+        end
+
+        predictedTrackInds = predictedTrackInds(predictedTrackInds > 0);
+
+        % Create labels for objects that display the predicted rather
+        % than the actual location.
+        labels = cellstr(int2str(ids));
+
+        isPredicted = cell(size(labels));
+        isPredicted(predictedTrackInds) = {' predicted'};
+        labels = strcat(labels,isPredicted);
+
+        % Draw the objects on the frame.
+        frame = insertObjectAnnotation(frame,"rectangle",boxes,labels);
+    end
+
+    % Display the mask and the frame.
+    videoPlayer.step(frame);
+end
+
 
 
 % % ---------------------------- Bubble Detection and tracking---------------------------- %
@@ -40,6 +79,7 @@ tracker = multiObjectTracker();
 for frameNum = 1:numFrames
     frame = read(video, frameNum);
     i = frameNum;
+    cropped_frame = imcrop(frame, cropRect);
 
     % ---- Crop/Binarize Image ---- %
     gray_frame = rgb2gray(frame);
@@ -47,37 +87,78 @@ for frameNum = 1:numFrames
     binary_frame = imbinarize(gray_frame, thresh); 
     binary_frame = imcrop(binary_frame, cropRect);
 
-    % ---- Regionprops method ----- %
-    labeledImage = bwlabel(binary_frame);
-    stats = regionprops(labeledImage, 'Centroid', 'MajorAxisLength','MinorAxisLength','Area', 'BoundingBox');
+    % Save cropped frame
+    frameFileName = fullfile(outputFolder, sprintf('Cropped frame_%d.jpg', frameNum));
+    imwrite(cropped_frame, frameFileName);
 
-    centers = cat(1, stats.Centroid);
-    bboxes = cat(1, stats.BoundingBox);
-    MajorAxisLength = [stats.MajorAxisLength].';
-    MinorAxisLength = [stats.MinorAxisLength].';
-    radii = (mean([MajorAxisLength MinorAxisLength], 2)/2);
-    area = [stats.Area].';
-    volume = (sum((4*pi*radii.^3)/3)).*(length_per_pixel^3);
+    % % ---- Regionprops method ----- %
+    % labeledImage = bwlabel(binary_frame);
+    % stats = regionprops(labeledImage, 'Centroid', 'MajorAxisLength','MinorAxisLength','Area', 'BoundingBox');
+    % 
+    % centers = cat(1, stats.Centroid);
+    % bboxes = cat(1, stats.BoundingBox);
+    % MajorAxisLength = [stats.MajorAxisLength].';
+    % MinorAxisLength = [stats.MinorAxisLength].';
+    % radii = (mean([MajorAxisLength MinorAxisLength], 2)/2);
+    % area = [stats.Area].';
+    % volume = (sum((4*pi*radii.^3)/3)).*(length_per_pixel^3);
+    % 
+    % centroid_data{i} = centers;
+    % radii_data{i} = radii;
+    % area_data{i} = area;
+    % volume_data{i} = volume;
 
 
-    centroid_data{i} = centers;
-    radii_data{i} = radii;
-    area_data{i} = area;
-    volume_data{i} = volume;
+    % time = frameNum * timestep;
+    % if ~isempty(centers)
+    %     detections = cell(size(centers, 1), 1);
+    %     for j = 1:size(centers, 1)
+    %         detections{j} = objectDetection(time, centers(j, :), ...
+    %             'MeasurementNoise', 10 * eye(2), ...
+    %             'ObjectAttributes', struct('BoundingBox', bboxes(j, :), 'radius', radii(j)));
+    %     end
+    %     tracks = tracker(detections, time); % Update tracks
+    % else
+    %     tracks = tracker([], time); % No detections in this frame
+    % end
+    % 
+    % % step(trackPlayer, frame);
+    % displayTrackingResults(trackPlayer,tracks,cropped_frame);
+    % 
+    % 
+    % if i > 50
+    %     break
+    % end
 
-    if ~isempty(centers)
-        detections = cell(size(centers, 1), 1);
-        for j = 1:size(centers, 1)
-            detections{j} = objectDetection(0, centers(j, :), ...
-                'MeasurementNoise', 10 * eye(2), ...
-                'ObjectAttributes', struct('BoundingBox', bboxes(j, :)));
-        end
-        tracks = tracker(detections, 0); % Update tracks
-    else
-        tracks = tracker([], 0); % No detections in this frame
-    end
+    % --- I'll choose to display a random track --- %
+    % track_ID = 210;
+    % if ~isempty(tracks) &&  track_ID <= size(tracks, 1)
+    %     track = tracks([tracks.TrackID] == track_ID);
+    %     x = track.State(1);
+    %     y = track.State(3);
+    %     box = tracks(track_ID).ObjectAttributes.BoundingBox;
+    % 
+    % 
+    %     imshow(cropped_frame)
+    %     hold on
+    %     viscircles([x,y], 10,'Color','b');
+    % 
+    %     visualization_subfolder = 'Bubble Tracking Visualization 2';
+    %     visualization_subfolder = fullfile(DataAnalysisFolder, visualization_subfolder);  
+    % 
+    %     if ~exist(visualization_subfolder, 'dir')
+    %         mkdir(visualization_subfolder);
+    %     end
+    % 
+    %     saveas(gcf, fullfile(visualization_subfolder, sprintf('0p5_Na2SO4_10mlmin_CA_3V_500fps_%d.png', frameNum)));
+    % 
+    %     hold off
+    % end
 
-     
+
+
+
+
 
     % % ---- Circular Hough Transform method (this is pretty shit keep it commented) ---- %
     % [centers, radii, metric] = imfindcircles(img,[1 10], 'ObjectPolarity','bright');
@@ -99,6 +180,8 @@ for frameNum = 1:numFrames
 
 
 end
+
+
 
 
 % % ---------------------------- Save Data to CSV file ---------------------------- %
