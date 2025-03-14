4edf9dbc5ac590ef4463b538f219fea5d9735b80
Kawapeachy <tk3104@columbia.edu>
2025-03-14 16:41:20 -0400

    Bubble Detection Updates
    Updated some code with more commenting for bubble detection

--- s1_Bubble_Detection.m
+++ s1_Bubble_Detection.m
@@ -1,8 +1,8 @@
-% ------------------------------ Specify System Variables ------------------------------ %
+%% ------------------------------ Specify System Variables ------------------------------ %%
 % Specify the path to your video file and the output folder
 videoFilePath = 'C:\Users\ameyo\OneDrive\Desktop\Research\Data\Flow through electrolyzer\Cell v2 - bigger windows\2025-02-21-Ni-Nimesh-0p5Na2SO4-10mlmin-new_cell_footagealignment\0p5_Na2SO4_10mlmin_CA_3V_Backlighting_500fps.mov'; % Replace with your video file path
-outputFolder = 'C:\Users\ameyo\OneDrive\Desktop\Research\Data\Flow through electrolyzer\Cell v2 - bigger windows\2025-02-21-Ni-Nimesh-0p5Na2SO4-10mlmin-new_cell_footagealignment\Bubble Analysis'; % Replace with your output folder path
-DataAnalysisFolder = 'C:\Users\ameyo\OneDrive\Desktop\Research\Data\Flow through electrolyzer\Cell v2 - bigger windows\2025-02-21-Ni-Nimesh-0p5Na2SO4-10mlmin-new_cell_footagealignment\Data Analysis';
+outputFolder = 'C:\Users\ameyo\OneDrive\Desktop\Research\Data\Flow through electrolyzer\Cell v2 - bigger windows\2025-02-21-Ni-Nimesh-0p5Na2SO4-10mlmin-new_cell_footagealignment\Bubble Analysis smol'; % Replace with your output folder path
+DataAnalysisFolder = 'C:\Users\ameyo\OneDrive\Desktop\Research\Data\Flow through electrolyzer\Cell v2 - bigger windows\2025-02-21-Ni-Nimesh-0p5Na2SO4-10mlmin-new_cell_footagealignment\Data Analysis smol';
 
 if ~exist(outputFolder, 'dir')
     mkdir(outputFolder);
@@ -13,7 +13,8 @@ if ~exist(DataAnalysisFolder, 'dir')
 end
 
 % Specify video properties
-cropRect = [335, 0, 585, 790];  % crop rect = [x, y, a, b], where crop starts from pixels (x,y) with a width of a and height of b
+% cropRect = [335, 0, 585, 790];  % crop rect = [x, y, a, b], where crop starts from pixels (x,y) with a width of a and height of b
+cropRect = [335, 650, 585, 140];  % crop rect = [x, y, a, b], where crop starts from pixels (x,y) with a width of a and height of b
 fps = 500;
 timestep = 1/fps;
 length_per_pixel = 2.1915e-5; %length/pixel
@@ -30,10 +31,12 @@ frame = readFrame(video);
 frames = zeros(100,size(frame,1),size(frame,2));
 means = uint8(squeeze(mean(frames,1)));
 
-tracker = multiObjectTracker('MaxNumTracks', 1500, 'AssignmentThreshold', 30,  'ConfirmationThreshold', [8 10], DeletionThreshold= 20);
+tracker = multiObjectTracker('MaxNumTracks', 1500, 'AssignmentThreshold', 10,  'ConfirmationThreshold', [8 10], DeletionThreshold= 50);
 
 
-% --------- Custom Function ------------ %
+%% --------- Helper Functions for Bubble Detection ------------ %%
+
+% This function is copied from MATLAB multiObjectTracker website
 function displayTrackingResults(videoPlayer,confirmedTracks,frame)
     if ~isempty(confirmedTracks)
         % Display the objects. If an object has not been detected
@@ -74,7 +77,7 @@ end
 
 
 
-% % ---------------------------- Bubble Detection and tracking---------------------------- %
+%% ---------------------------- Bubble Detection and tracking---------------------------- %%
 % 
 for frameNum = 1:numFrames
     frame = read(video, frameNum);
@@ -87,96 +90,97 @@ for frameNum = 1:numFrames
     binary_frame = imbinarize(gray_frame, thresh); 
     binary_frame = imcrop(binary_frame, cropRect);
 
-    % Save cropped frame
-    frameFileName = fullfile(outputFolder, sprintf('Cropped frame_%d.jpg', frameNum));
-    imwrite(cropped_frame, frameFileName);
+    % ---- Regionprops method for Bubble Detection----- %
+    labeledImage = bwlabel(binary_frame);
+    stats = regionprops(labeledImage, 'Centroid', 'MajorAxisLength','MinorAxisLength','Area', 'BoundingBox');
 
-    % % ---- Regionprops method ----- %
-    % labeledImage = bwlabel(binary_frame);
-    % stats = regionprops(labeledImage, 'Centroid', 'MajorAxisLength','MinorAxisLength','Area', 'BoundingBox');
+    centers = cat(1, stats.Centroid);
+    bboxes = cat(1, stats.BoundingBox);
+    MajorAxisLength = [stats.MajorAxisLength].';
+    MinorAxisLength = [stats.MinorAxisLength].';
+    radii = (mean([MajorAxisLength MinorAxisLength], 2)/2);
+    area = [stats.Area].';
+    volume = (sum((4*pi*radii.^3)/3)).*(length_per_pixel^3);
+
+    centroid_data{i} = centers;
+    radii_data{i} = radii;
+    area_data{i} = area;
+    volume_data{i} = volume;
+
+    % % ---- Circular Hough Transform method for Bubble Detection ---- %
+    % [centers, radii, metric] = imfindcircles(img,[1 10], 'ObjectPolarity','bright');
+    
+    % % ---- Visualize Bubble Detection ----% 
+    % imshow(imcrop(read(video, i), cropRect))
+    % hold on;
+    % viscircles(centers, radii,'Color','b');
+    % hold off
     % 
-    % centers = cat(1, stats.Centroid);
-    % bboxes = cat(1, stats.BoundingBox);
-    % MajorAxisLength = [stats.MajorAxisLength].';
-    % MinorAxisLength = [stats.MinorAxisLength].';
-    % radii = (mean([MajorAxisLength MinorAxisLength], 2)/2);
-    % area = [stats.Area].';
-    % volume = (sum((4*pi*radii.^3)/3)).*(length_per_pixel^3);
+    % visualization_subfolder = 'Bubble Detection Visualization';
+    % visualization_subfolder = fullfile(DataAnalysisFolder, visualization_subfolder);  
     % 
-    % centroid_data{i} = centers;
-    % radii_data{i} = radii;
-    % area_data{i} = area;
-    % volume_data{i} = volume;
-
-
-    % time = frameNum * timestep;
-    % if ~isempty(centers)
-    %     detections = cell(size(centers, 1), 1);
-    %     for j = 1:size(centers, 1)
-    %         detections{j} = objectDetection(time, centers(j, :), ...
-    %             'MeasurementNoise', 10 * eye(2), ...
-    %             'ObjectAttributes', struct('BoundingBox', bboxes(j, :), 'radius', radii(j)));
-    %     end
-    %     tracks = tracker(detections, time); % Update tracks
-    % else
-    %     tracks = tracker([], time); % No detections in this frame
+    % if ~exist(visualization_subfolder, 'dir')
+    %     mkdir(visualization_subfolder);
     % end
     % 
-    % % step(trackPlayer, frame);
-    % displayTrackingResults(trackPlayer,tracks,cropped_frame);
-    % 
-    % 
-    % if i > 50
-    %     break
-    % end
+    % saveas(gcf, fullfile(visualization_subfolder, sprintf('0p5_Na2SO4_10mlmin_CA_3V_500fps_%d.png', frameNum)));
 
-    % --- I'll choose to display a random track --- %
-    % track_ID = 210;
-    % if ~isempty(tracks) &&  track_ID <= size(tracks, 1)
+
+
+    % ---- multiObjectTracker for Bubble Tracking ---- %
+    time = frameNum * timestep;
+    if ~isempty(centers)
+        detections = cell(size(centers, 1), 1);
+        for j = 1:size(centers, 1)
+            detections{j} = objectDetection(time, centers(j, :), ...
+                'MeasurementNoise', 10 * eye(2), ...
+                'ObjectAttributes', struct('BoundingBox', bboxes(j, :), 'radius', radii(j)));
+        end
+        tracks = tracker(detections, time); % Update tracks
+    else
+        tracks = tracker([], time); % No detections in this frame
+    end
+
+    % ---- Visualize Bubble Tracking ---- %
+    % All tracks
+    displayTrackingResults(trackPlayer,tracks,cropped_frame); s
+
+    % % Specified Track
+    % trackIDs = [tracks.TrackID];
+    % track_ID = 56;
+    % if ~isempty(tracks) &&  track_ID <= size(tracks, 1) && any(trackIDs == track_ID)
     %     track = tracks([tracks.TrackID] == track_ID);
     %     x = track.State(1);
     %     y = track.State(3);
     %     box = tracks(track_ID).ObjectAttributes.BoundingBox;
+    %     length = track.ObjectAttributes.radius;
     % 
     % 
     %     imshow(cropped_frame)
     %     hold on
-    %     viscircles([x,y], 10,'Color','b');
     % 
-    %     visualization_subfolder = 'Bubble Tracking Visualization 2';
-    %     visualization_subfolder = fullfile(DataAnalysisFolder, visualization_subfolder);  
-    % 
-    %     if ~exist(visualization_subfolder, 'dir')
-    %         mkdir(visualization_subfolder);
+    %     if track.IsCoasted
+    %         % viscircles([x,y], 10,'Color','b');
+    %         rectangle('Position', [x - length, y - length, length*2, length*2], 'EdgeColor', 'b', 'LineWidth', 0.5, 'FaceAlpha', 0.3)
+    %         hold off
+    %     else
+    %         % viscircles([x,y], 10,'Color','r');
+    %         rectangle('Position', [x - length, y - length, length*2, length*2], 'EdgeColor', 'r', 'LineWidth', 0.5, 'FaceAlpha', 0.3)
+    %         hold off
     %     end
+    %
+    %     % % Save into folder
+    %     % visualization_subfolder = 'Bubble Tracking Visualization'; 
+    %     % visualization_subfolder = fullfile(DataAnalysisFolder, visualization_subfolder);  
+    %     % 
+    %     % if ~exist(visualization_subfolder, 'dir')
+    %     %     mkdir(visualization_subfolder);
+    %     % end
+    %     % 
+    %     % saveas(gcf, fullfile(visualization_subfolder, sprintf('0p5_Na2SO4_10mlmin_CA_3V_500fps_%d.png', frameNum)));
     % 
-    %     saveas(gcf, fullfile(visualization_subfolder, sprintf('0p5_Na2SO4_10mlmin_CA_3V_500fps_%d.png', frameNum)));
     % 
-    %     hold off
     % end
-
-
-
-
-
-
-    % % ---- Circular Hough Transform method (this is pretty shit keep it commented) ---- %
-    % [centers, radii, metric] = imfindcircles(img,[1 10], 'ObjectPolarity','bright');
-    
-%     % ---- Visualize image ----% 
-%     imshow(imcrop(read(video, i), cropRect))
-%     hold on;
-%     viscircles(centers, radii,'Color','b');
-%     hold off
-% 
-%     visualization_subfolder = 'Bubble Tracking Visualization';
-%     visualization_subfolder = fullfile(DataAnalysisFolder, visualization_subfolder);  
-% 
-%     if ~exist(visualization_subfolder, 'dir')
-%         mkdir(visualization_subfolder);
-%     end
-% 
-%     saveas(gcf, fullfile(visualization_subfolder, sprintf('0p5_Na2SO4_10mlmin_CA_3V_500fps_%d.png', frameNum)));
 
 
 end
@@ -184,7 +188,7 @@ end
 
 
 
-% % ---------------------------- Save Data to CSV file ---------------------------- %
+%% ---------------------------- Save Data to CSV file ---------------------------- %%
 
 % % --- volume data --- %
 % % volume_data = cell2mat(volume_data);
@@ -199,7 +203,7 @@ end
 % end
 
 
-% % ----------- Data Analysis ------------- %
+%% ----------- Quick Data Analysis ------------- %%
 % % volume_data = cell2mat(volume_data);
 % 
 % % plot Histogram
@@ -223,36 +227,3 @@ end
 % hold off
 % saveas(gcf, fullfile(DataAnalysisFolder, 'Volume_scatter_plot.png'));
 % 
-% 
-
-
-% ---------------------------- Bubble Tracking ---------------------------- %
-
-% Point Tracker
-% % imageFiles = dir(fullfile(outputFolder, '*.png')); 
-% % numFiles = length(imageFiles);
-% 
-% initial_frame = read(video, 1);
-% initial_frame = imcrop(initial_frame, cropRect);
-% 
-% PointTracker = vision.PointTracker;
-% initialize(PointTracker, centroid_data{1}, initial_frame);
-% 
-% for i = 2:numFrames
-%     frame = read(video, i);
-%     frame = imcrop(frame, cropRect);
-% 
-%     [points, validity] = step(PointTracker, frame);
-%     dx = diff(points(:,1));
-%     dy = diff(points(:,2));
-%     velocity = sqrt(dx.^2 + dy.^2) / 0.002;  % timeStep depends on frame rate
-% 
-%     frame = insertMarker(frame, points, 'color', 'red', 'size', 1);
-% 
-%     velocity_data{i-1} = velocity;
-% 
-%     imshow(frame);
-% 
-%     pause
-% 
-% end
